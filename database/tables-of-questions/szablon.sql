16e769568123d84087d906fa5f2196015d7fa07a
Jakub <79814706+Furbysz@users.noreply.github.com>
2021-11-23 15:45:13 +0100

    SQL 1.11
    Poprawiono nazewnictwo, dodano szablon

---
+++ database/tables-of-questions/szablon.sql
@@ -0,0 +1,522 @@
+# Czerwiec 2021 E14
+# arkusz -> https://arkusze.pl/zawodowy/e14-2021-czerwiec-egzamin-zawodowy-pisemny.pdf
+# ODP -> https://arkusze.pl/zawodowy/e14-2021-czerwiec-egzamin-zawodowy-pisemny-odpowiedzi.pdf
+INSERT INTO ee09 
+(#nr_pyt
+id_arkusza,
+prepytanie,
+pytanie, 
+odp1, 
+odp2, 
+odp3, 
+odp4, 
+poprawna,
+Qtype,
+Atype,
+files
+) VALUES 
+
+(#1
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#2
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#3
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#4
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#5
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#6
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#7
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#8
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#9
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#10
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#11
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#12
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#13
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#14
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#15
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#16
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#17
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#18
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#19
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#20
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#21
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#22
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#23
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#24
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#25
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#26
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#27
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#28
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#28
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#29
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#30
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#1
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#31
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#32
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#33
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#34
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#35
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#36
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#37
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#38
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#39
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+),
+(#40
+1,  #id Arkusza
+'', #prepyt
+'', #odp1
+'', #odp2
+'', #odp3
+'', #odp4
+'', #poprawna
+'', #Qtype
+'', #Atype
+1   #files
+);
