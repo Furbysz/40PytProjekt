ddbf1bebfbd03d717b580a3396665f76b2465fad
Jakub <79814706+Furbysz@users.noreply.github.com>
2021-12-02 09:40:58 +0100

    1.6.1
    reload.php dalej nie działczy :<

--- database/reload.php
+++ database/reload.php
@@ -10,27 +10,56 @@ echo '<hr>';
 */
 
 $kwerendy = array();
-$kwerendy[0] = 'DROP DATABASE pytania';
-$pliki[0] = '#DROP DB';
-$kwerendy[1] = 'CREATE DATABASE pytania';
-$pliki[1] = '#CREATE DB';
-$j = 3;
+$kwDrop = 'DROP DATABASE pytania';
+$kwCreate = 'CREATE DATABASE pytania';
+$j = 0;
 foreach($dir as $plik){
     if($plik == 'reload.php'){continue;}
     if($plik == '.' OR $plik == '..'){continue;}
     if(str_starts_with($plik, '[TEAMPLATE]')){continue;}
-    if(str_starts_with($plik, '[MASTER]'))
-    {
-        $kwerendy[2] = file_get_contents($plik);
-        $pliki[2] = $plik; 
-        continue;
-    }
+    if(str_starts_with($plik, '[MASTER]')){$master = file_get_contents($plik); continue;}
     
     $pliki[$j] = $plik;
     $kwerendy[$j] = file_get_contents($plik);
     $j++;
 }
 // Wykonaj
+//DROP
+if(mysqli_query($dbc, $kwDrop)){
+        echo '<b>USUNIĘTO BAZĘ</b><br>';
+    } else {
+        echo '<b>BŁĄD USUWANIA BAZY</b><br>';
+    }
+//CREATE
+if(mysqli_query($dbc, $kwCreate)){
+        echo '<b>UTWORZONO BAZĘ</b><br>';
+    } else {
+        echo '<b>BŁĄD TWORZENIA BAZY</b><br>';
+    }
+//SELECT
+if(mysqli_select_db($dbc, 'pytania')){
+    echo '<b>POŁĄCZONO Z NOWĄ BAZĄ</b><br>';
+} else {
+    echo '<b>BŁĄD ŁĄCZENIA Z NOWĄ BAZĄ</b><br>';
+}
+//MASTER
+if(mysqli_query($dbc, $master)){
+    echo '<b>master.sql wykonane</b><br>';
+} else {
+    echo '<b>BŁĄD wykonania master.sql</b><br>';
+    echo '<pre>';
+    print_r(mysqli_error($dbc));
+    echo '<br>';
+    echo '</pre>';
+    print_r($master);
+    
+    /*
+    echo '<pre>';
+    print_r($master);
+    echo '</pre>';
+    */
+}
+
 $ile_plikow = count($kwerendy);
 for($i = 0; $i < $ile_plikow; $i++){
     echo '<br><b>'.$pliki[$i].'</b><br>';
